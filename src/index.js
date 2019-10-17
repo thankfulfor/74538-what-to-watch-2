2e995bd035ada6dc96a08c8c8f9c9ac1fb7bf54e
Жанна Нарсия <z.narsija@superjob.ru>
2019-10-17 22:03:23 +0300

    5. Личный проект: начинаем программировать

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from './components/app/app.jsx';
+
+const init = () => {
+  ReactDOM.render(
+      <App />,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
